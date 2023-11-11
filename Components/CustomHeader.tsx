d36a78b5c1f360fcc1fe705b022990073278bd79
hugop <hugopinho07@gmail.com>
2023-11-11 10:48:03 -0300

    first commit

---
+++ Components/CustomHeader.tsx
@@ -0,0 +1,135 @@
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useRef } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../constants/Colors";
+import { Link } from "expo-router";
+import BottomSheet from "./BottomSheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+
+const SearchBar = () => {
+  return (
+    <View style={styles.searchContainer}>
+      <View style={styles.searchSection}>
+        <View style={styles.searchField}>
+          <Ionicons
+            style={styles.searchIcon}
+            name="ios-search"
+            size={20}
+            color={Colors.medium}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Restaurantes, groceries, dishes"
+          />
+        </View>
+        <Link href={"/(modal)/filter"} asChild>
+          <TouchableOpacity style={styles.optionButton}>
+            <Ionicons name="options-outline" size={20} color={Colors.primary} />
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+const CustomHeader = () => {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+  const openModal = () => {
+    bottomSheetRef.current?.present();
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <BottomSheet ref={bottomSheetRef} />
+      <View style={styles.container}>
+        <TouchableOpacity onPress={openModal}>
+          <Image
+            style={styles.bike}
+            source={require("../assets/images/bike.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={openModal} style={styles.titleContainer}>
+          <Text style={styles.title}>Delivery . Now</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Text style={styles.subtitle}>San Francisco</Text>
+            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name="person-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </View>
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+export default CustomHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 60,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 20,
+    paddingHorizontal: 20,
+  },
+  bike: { width: 30, height: 30 },
+  titleContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  searchContainer: {
+    height: 60,
+    backgroundColor: "#fff",
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    // flex: 1,
+    paddingHorizontal: 20,
+    alignItem: "center",
+  },
+
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  input: { padding: 10, color: Colors.mediumDark },
+  searchIcon: { paddingLeft: 10 },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { Text, ScrollView, StyleSheet } from "react-native";
+import React from "react";
+import Categories from "../Components/Categories";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Restaurants from "../Components/Restaurants";
+import Colors from "../constants/Colors";
+
+const Page = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighbourhood</Text>
+        <Restaurants />
+        <Text style={styles.header}>Offers near you</Text>
+        <Restaurants />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({
+  container: { top: 60, backgroundColor: Colors.lightGrey },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+});
